b1089f34c80eb28f0dcd9680688b457bb0e0e8b3
Viktoriia <mw3feria@gmail.com>
2020-09-17 02:20:10 +0200

    My portfolio

---
+++ precache-manifest.c7b682e5d62f91070e9e40a5b28c3392.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f521d1f2c44892e13df780b14ed5eff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec0a4189667779666490",
+    "url": "/static/css/main.9f6be48a.chunk.css"
+  },
+  {
+    "revision": "326677cd707da4f8eb16",
+    "url": "/static/js/2.32e4c242.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/static/js/2.32e4c242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0a4189667779666490",
+    "url": "/static/js/main.545b207b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7c0f5d4be0f30baa7015dcaa06c09977",
+    "url": "/static/media/code-min.7c0f5d4b.JPG"
+  },
+  {
+    "revision": "bd3ea6952c1e3e36aab77a20f7e79700",
+    "url": "/static/media/ok.bd3ea695.svg"
+  },
+  {
+    "revision": "aec39d69a3013f0cbf0ae923169a7f57",
+    "url": "/static/media/viktoriia_aqua-min.0c20c31d.png"
+  }
+]);
